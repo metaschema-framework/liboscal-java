29f2c58bb8be76be3021e615fcbf4f396385c0a2
David Waltermire <davewaltermire@gmail.com>
2025-12-11 21:43:22 -0500

    fix: update test for metaschema-java PR #529 compatibility (#220)
    * fix: update test for metaschema-java PR #529 compatibility
    
    Replace removed XMLBeans-based classes with binding-based alternatives:
    - XmlMetaConstraintLoader -> BindingConstraintLoader
    - ModuleLoader -> IBindingModuleLoader
    - IXmlMetaschemaModule -> IBindingMetaschemaModule
    
    The metaschema-java PR #529 removed the core.model.xml package, requiring
    migration to the databind module's binding-based loaders.
    
    * chore: bump OSCAL submodule to latest develop

--- src/test/java/gov/nist/secauto/oscal/lib/model/util/AllowedValueCollectingNodeItemVisitorTest.java
+++ src/test/java/gov/nist/secauto/oscal/lib/model/util/AllowedValueCollectingNodeItemVisitorTest.java
@@ -8,14 +8,10 @@ package gov.nist.secauto.oscal.lib.model.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import gov.nist.secauto.metaschema.core.model.MetaschemaException;
-import gov.nist.secauto.metaschema.core.model.constraint.ExternalConstraintsModulePostProcessor;
 import gov.nist.secauto.metaschema.core.model.constraint.IConstraintSet;
-import gov.nist.secauto.metaschema.core.model.xml.IXmlMetaschemaModule;
-import gov.nist.secauto.metaschema.core.model.xml.ModuleLoader;
-import gov.nist.secauto.metaschema.core.model.xml.XmlMetaConstraintLoader;
-import gov.nist.secauto.metaschema.core.util.CollectionUtil;
 import gov.nist.secauto.metaschema.core.util.ObjectUtils;
 import gov.nist.secauto.metaschema.databind.IBindingContext;
+import gov.nist.secauto.metaschema.databind.model.metaschema.BindingConstraintLoader;
 import gov.nist.secauto.metaschema.databind.model.metaschema.IBindingMetaschemaModule;
 import gov.nist.secauto.metaschema.databind.model.metaschema.IBindingModuleLoader;
 import gov.nist.secauto.oscal.lib.model.util.AllowedValueCollectingNodeItemVisitor.NodeItemRecord;
@@ -33,11 +29,21 @@ class AllowedValueCollectingNodeItemVisitorTest {
 
   @Test
   void testAllowedValuesMissingVariableBindings() throws MetaschemaException, IOException {
-    List<IConstraintSet> constraintSet = new XmlMetaConstraintLoader().load(ObjectUtils.requireNonNull(
+    IBindingContext bindingContext = IBindingContext.builder().build();
+
+    List<IConstraintSet> constraintSet = new BindingConstraintLoader(bindingContext).load(ObjectUtils.requireNonNull(
         Paths.get("src/test/resources/content/issue-112/computer-metaschema-meta-constraints.xml").toUri()));
-    ExternalConstraintsModulePostProcessor postProcessor = new ExternalConstraintsModulePostProcessor(constraintSet);
-    ModuleLoader loader = new ModuleLoader(CollectionUtil.singletonList(postProcessor));
-    IXmlMetaschemaModule module = loader.load(ObjectUtils.requireNonNull(
+
+    Path genDir = Paths.get("target/dynamic-bindings/allowed-values-1");
+    Files.createDirectories(genDir);
+
+    IBindingContext bindingContextWithConstraints = IBindingContext.builder()
+        .constraintSet(constraintSet)
+        .compilePath(genDir)
+        .build();
+
+    IBindingModuleLoader loader = bindingContextWithConstraints.newModuleLoader();
+    IBindingMetaschemaModule module = loader.load(ObjectUtils.requireNonNull(
         Paths.get("src/test/resources/content/issue-112/computer-example.xml").toUri()));
     AllowedValueCollectingNodeItemVisitor walker = new AllowedValueCollectingNodeItemVisitor();
     walker.visit(module);
@@ -47,10 +53,13 @@ class AllowedValueCollectingNodeItemVisitorTest {
 
   @Test
   void testAllowedValuesMissingVariableBindings2() throws MetaschemaException, IOException {
-    List<IConstraintSet> constraintSet = new XmlMetaConstraintLoader().load(ObjectUtils.requireNonNull(
-        Paths.get("src/test/resources/content/issue-112/computer-metaschema-meta-constraints.xml").toUri()));
+    IBindingContext initialBindingContext = IBindingContext.builder().build();
 
-    Path genDir = Paths.get("target/dynamic-bindings/allowed-values");
+    List<IConstraintSet> constraintSet = new BindingConstraintLoader(initialBindingContext).load(ObjectUtils
+        .requireNonNull(Paths.get("src/test/resources/content/issue-112/computer-metaschema-meta-constraints.xml")
+            .toUri()));
+
+    Path genDir = Paths.get("target/dynamic-bindings/allowed-values-2");
     Files.createDirectories(genDir);
 
     IBindingContext bindingContext = IBindingContext.builder()
